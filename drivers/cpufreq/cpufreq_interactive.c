4eaf4b5ae3ca7b99bb0bf8e85702c66a18973cf5
geiti94 <geiti94@me.com>
2016-10-30 19:59:47 +0100

    Revert "Tweaked hotplug in combination with Interactive for a max freq. Credits to Imosoyen"
    This reverts commit f676ba4f1199c057d902952e45dce6ca79e91baf.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -51,8 +51,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
 
-extern bool screen_on;
-
 struct cpufreq_interactive_cpuinfo {
 	struct timer_list cpu_timer;
 	struct timer_list cpu_slack_timer;
@@ -102,10 +100,6 @@ static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
 
-
-
-#define DEFAULT_SCREEN_OFF_MAX 1000000
-static unsigned long screen_off_max = DEFAULT_SCREEN_OFF_MAX;
 
 struct cpufreq_interactive_tunables {
 	int usage_count;
@@ -797,9 +791,6 @@ static int cpufreq_interactive_speedchange_task(void *data)
 				pjcpu = &per_cpu(cpuinfo, j);
 				pjcpu->floor_validate_time = fvt;
 			}
-			
-			if (unlikely(!screen_on))
-					if (max_freq > screen_off_max) max_freq = screen_off_max;
 
 			if (max_freq != pcpu->policy->cur) {
 				__cpufreq_driver_target(pcpu->policy,
@@ -2004,7 +1995,6 @@ static int __init cpufreq_interactive_init(void)
 {
 	unsigned int i;
 	struct cpufreq_interactive_cpuinfo *pcpu;
-	screen_on = true;
 
 	/* Initalize per-cpu timers */
 	for_each_possible_cpu(i) {
--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -54,8 +54,6 @@ static DEFINE_MUTEX(cluster0_hotplug_in_lock);
 static DEFINE_MUTEX(thread_manage_lock);
 #endif
 
-bool screen_on = true;
-
 static struct task_struct *dm_hotplug_task;
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
 static bool thread_start = false;
@@ -449,7 +447,6 @@ static int fb_state_change(struct notifier_block *nb,
 	case FB_BLANK_POWERDOWN:
 		lcd_is_on = false;
 		pr_info("LCD is off\n");
-		screen_on = false;
 
 		delay = POLLING_MSEC_DISP_OFF;
 
@@ -470,7 +467,6 @@ static int fb_state_change(struct notifier_block *nb,
 		 */
 		lcd_is_on = true;
 		pr_info("LCD is on\n");
-		screen_on = true;
 
 		delay = POLLING_MSEC_DISP_ON;
 
